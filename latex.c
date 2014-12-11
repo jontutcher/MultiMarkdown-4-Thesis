9da6effce5396aef46dbf9c105ad8bb7e999c2c6
Jon Tutcher <jon@jtutcher.co.uk>
2014-12-11 13:04:21 +0000

    Syntax highlighting changed to use 'minted'

--- latex.c
+++ latex.c
@@ -167,7 +167,9 @@ void print_latex_node(GString *out, node *n, scratch_pad *scratch) {
 			if ((n->children != NULL) && (n->children->key == VERBATIMTYPE)) {
 				trim_trailing_whitespace(n->children->str);
 				if (strlen(n->children->str) > 0) {
-					g_string_append_printf(out, "\\begin{lstlisting}[language=%s]\n%s\\end{lstlisting}", n->children->str,n->str);
+					//use minted package rather than lstlisting for code listing
+					g_string_append_printf(out, "\\begin{minted}{%s}\n%s\\end{minted}", n->children->str,n->str);
+					// g_string_append_printf(out, "\\begin{lstlisting}[language=%s]\n%s\\end{lstlisting}", n->children->str,n->str);
 					scratch->padded = 0;
 					break;
 				}
